1c63e5ab8da28222dd7925382c296c2e64f89d60
Simon <simon.prast.sp@gmail.com>
2020-12-11 13:37:24 +0100

    Add ChangeUserSerializer user data validation
    Add custom validation of user data to the ChangeUserSerializer.
    
    The data is validated using user.create_or_login.validated_user_data.
    
    The serializer defines an update function, which uses the serializer's fields in order to update the user instance.
    
    user.create_or_login.validated_user_data now skips the validation whether a field is required or not when stated that a user instance is only changed.

--- user/api/dev/serializers.py
+++ user/api/dev/serializers.py
@@ -51,6 +51,24 @@ class ChangeUserSerializer(serializers.ModelSerializer):
     email = serializers.CharField(required=False)
     phone = serializers.CharField(required=False)
 
+    def validate(self, value):
+        # If the request method is PUT it must be stated that the object
+        # is only changed and the fields will therefore only be optional.
+        change = True if self.context.get('request').method == 'PUT' else False
+
+        # Ensure that the given user arguments are valid and set the values accordingly
+        self.first_name, self.last_name, self.email, self.phone, password = validated_user_data(
+            self.initial_data, change=change)
+        return value
+
+    def update(self, instance, validated_data):
+        instance.first_name = self.first_name or instance.first_name
+        instance.last_name = self.last_name or instance.last_name
+        instance.email = self.email or instance.email
+        instance.phone = self.phone or instance.phone
+        instance.save()
+        return instance
+
     class Meta:
         model = User
         fields = [
