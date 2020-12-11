693fc93c2b6ba9c44b6afe4e65a319058b41df24
Simon <simon.prast.sp@gmail.com>
2020-12-11 11:44:41 +0100

    Add new modules API URLs to root API URLs
    Add the API endpoint URLS of the new modules to the root API URLs file at api.dev.urls:
    - damagereport/ for submission.damagereport.api.dev.urls
    - id/ for submission.id.api.dev.urls
    - insurance/ for submission.insurancesubmission.api.dev.urls
    
    The old endpint, submit/, is removed.

--- api/dev/urls.py
+++ api/dev/urls.py
@@ -11,5 +11,7 @@ from django.urls import path, include
 urlpatterns = [
     # This URLs file is routing the requests to each module's own URLs file
     path('users/', include('user.api.dev.urls')),
-    path('submit/', include('submission.api.dev.urls'))
+    path('damagereport/', include('submission.damagereport.api.dev.urls')),
+    path('id/', include('submission.id.api.dev.urls')),
+    path('insurance/', include('submission.insurancesubmission.api.dev.urls')),
 ]
