2884ee4fa6b124bfeb005924744783c6d7fd97e7
Simon <simon.prast.sp@gmail.com>
2020-12-22 13:04:45 +0100

    Change v1 API URL paths
    The v1 API URLs at api.1.urls were still linking to the dev api url's.

--- api/1/urls.py
+++ api/1/urls.py
@@ -10,8 +10,8 @@ from django.urls import path, include
 
 urlpatterns = [
     # This URLs file is routing the requests to each module's own URLs file
-    path('users/', include('user.api.dev.urls')),
-    path('damagereport/', include('submission.damagereport.api.dev.urls')),
-    path('id/', include('submission.id.api.dev.urls')),
-    path('insurance/', include('submission.insurancesubmission.api.dev.urls')),
+    path('users/', include('user.api.1.urls')),
+    path('damagereport/', include('submission.damagereport.api.1.urls')),
+    path('id/', include('submission.id.api.1.urls')),
+    path('insurance/', include('submission.insurancesubmission.api.1.urls')),
 ]
